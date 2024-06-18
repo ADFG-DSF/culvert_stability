dbf014201c6a44ed2c55f0075aaa6084d68887cd
Matt Tyers <matt.tyers@alaska.gov>
2024-06-18 15:52:08 -0800

    added some lm & rpart ideas

--- R/xx_randomforest.R
+++ R/xx_randomforest.R
@@ -34,7 +34,95 @@ lm_all <- function(x, y, maxnumber = 5) {
   }
   return(the_lms)
 }
-aa <- lm_all(y=Vnew[,1], x=Designs_subset[,-c(1,2,3,9,27,28,31,32)])
+
+lm_all_data <- Designs_subset[,-c(1,2,3,9,27,28,29,31,32)]
+lm_all_y <- cbind(Vnew[,1:3], pca_out$PCA_banks_1)
+
+all_lm_all <- list()
+for(i in 1:4) {
+  the_lm_all_call <- lm_all(y=lm_all_y[,i], x=lm_all_data, maxnumber=5)
+  all_lm_all[[i]] <- the_lm_all_call[[which.min(sapply(the_lm_all_call, AIC))]]
+}
+names(all_lm_all)[1:3] <- names(Vnew)[1:3]
+names(all_lm_all)[4] <- "PCAbanks1"
+
+# the_lm_all_call <- lm_all(pca_out$PCA_banks_1, x=lm_all_data, maxnumber=5)
+# all_lm_all[[4]] <- the_lm_all_call[[which.min(sapply(the_lm_all_call, AIC))]]
+
+
+for(i in 1:length(all_lm_all)) {
+  print(names(all_lm_all)[i])
+  print(summary(all_lm_all[[i]]))
+}
+
+
+library(rpart)
+library(rpart.plot)
+trees <- list()
+for(i in 1:4) {
+  trees[[i]] <- rpart(lm_all_y[,i] ~ lake_outlet +
+          straight_or_meandering +
+          banks_y_n_9 +
+          features_y_n +
+          one_size_material_used_for_all +
+          reach_3_structure_width +
+          reach3_design_cr +
+          reach_3_total_length +
+          reach_3_gradient_50 +
+          reach_3_average_inlet_and_outlet_height +
+          reach_3_inlet_h +
+          reach_3_inlet_w +
+          reach_3_outlet_h +
+          reach_3_outlet_w +
+          reach_3_geo_channel_type +
+          reach_3_channel_form +
+          reach_3_no_of_meanders +
+          low_flow_y_n_60 +
+          x3_feature_numer +
+          reach_3_interior_banks_y_n +
+          bank_design_type +
+          x3_width_straight +
+          design_cr, data=lm_all_data)
+}
+# trees[[4]] <- rpart(pca_out$PCA_banks_1 ~ lake_outlet +
+#         straight_or_meandering +
+#         banks_y_n_9 +
+#         features_y_n +
+#         one_size_material_used_for_all +
+#         reach_3_structure_width +
+#         reach3_design_cr +
+#         reach_3_total_length +
+#         reach_3_gradient_50 +
+#         reach_3_average_inlet_and_outlet_height +
+#         reach_3_inlet_h +
+#         reach_3_inlet_w +
+#         reach_3_outlet_h +
+#         reach_3_outlet_w +
+#         reach_3_geo_channel_type +
+#         reach_3_channel_form +
+#         reach_3_no_of_meanders +
+#         low_flow_y_n_60 +
+#         x3_feature_numer +
+#         reach_3_interior_banks_y_n +
+#         bank_design_type +
+#         x3_width_straight +
+#         design_cr, data=lm_all_data)
+
+par(mfrow=c(1,2))
+for(i in 1:4) {
+  plot(fitted(trees[[i]]), lm_all_y[,i])
+  # plot(fitted(all_lm_all[[i]]), lm_all_y[,i])
+}
+
+
+##### idea: compare candidate models (regtree included) in terms of
+##### loocv predictive accuracy (rmse of predicted vs actual)
+
+## formula(all_lm_all[[1]]) gets you the formula
+
+
+lm_all_InteriorChannelWidth <- lm_all(y=Vnew$`Interior Channel Width`,
+             x=lm_all_data)
 theAIC <- sapply(aa, AIC)
 summary(aa[[which.min(theAIC)]])
 par(mfrow=c(2,2))
